55334965455205a2a9d5a12ea87c2df2a78ad995
Brian Dwyer <Brian.Dwyer@broadridge.com>
2020-04-15 12:12:43 -0400

    Add a basic test
    Signed-off-by: Brian Dwyer <Brian.Dwyer@broadridge.com>

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.5.0
+	github.com/stretchr/testify v1.2.2
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.2.8
---
+++ main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefault(t *testing.T) {
+	cwd, err := os.Getwd()
+	assert.Nil(t, err)
+	os.Args = []string{"cmd", "-config=" + filepath.Join(cwd, "test", "config.yml")}
+
+	// Hack! :-)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go func() {
+		go main()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	// Wait a second for the Server
+	time.Sleep(1 * time.Second)
+
+	resp, err := http.Get("http://localhost:3456")
+	assert.Nil(t, err)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	assert.Nil(t, err)
+
+	var status GlobalStatus
+
+	err = json.Unmarshal([]byte(body), &status)
+	assert.Nil(t, err, "Status should be a valid JSON GlobalStatus")
+
+	assert.Equal(t, true, status.Healthy, "Global status should be healthy")
+}
