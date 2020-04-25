143bcf25f539d981a527515992b9f64313ef89e2
Brian Dwyer <Brian.Dwyer@broadridge.com>
2020-04-25 14:17:34 -0400

    feat: Add support for RedHat and Debian SysV services
    Signed-off-by: Brian Dwyer <Brian.Dwyer@broadridge.com>

--- service_other.go
+++ service_other.go
@@ -3,6 +3,11 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
 	log "github.com/sirupsen/logrus"
 
 	"github.com/coreos/go-systemd/v22/dbus"
@@ -15,6 +20,19 @@ type SvcStatus struct {
 }
 
 func (svc *Service) Check() (status SvcStatus, err error) {
+	switch {
+	case isSystemD():
+		return svc.checkSystemD()
+	case isRedhatSysV(), isDebianSysV():
+		return svc.checkSysV()
+	default:
+		log.Fatal("Service checks on this platform are not supported yet!")
+	}
+
+	return
+}
+
+func (svc *Service) checkSystemD() (status SvcStatus, err error) {
 	status.Name = svc.Name
 
 	m, err := dbus.New()
@@ -40,4 +58,50 @@ func (svc *Service) Check() (status SvcStatus, err error) {
 	}
 
 	return
+}
+
+func (svc *Service) checkSysV() (status SvcStatus, err error) {
+	status.Name = svc.Name
+
+	status.State = make(map[string]interface{})
+	cmd := exec.Command("service", svc.Name, "status")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		cmdString := strings.Join([]string{"service", svc.Name, "status"}, " ")
+		err = fmt.Errorf("%q failed: %v, %s", cmdString, err, out)
+	}
+
+	output := strings.TrimSpace(string(out))
+	status.State["Output"] = output
+
+	if strings.Contains(output, "is running") {
+		status.Healthy = true
+	}
+
+	return
+}
+
+func isSystemD() bool {
+	// https://www.freedesktop.org/software/systemd/man/sd_booted.html
+	if _, err := os.Stat("/run/systemd/system/"); err != nil {
+		return false
+	}
+	return true
+}
+
+func isDebianSysV() bool {
+	if _, err := os.Stat("/lib/lsb/init-functions"); err != nil {
+		return false
+	}
+	if _, err := os.Stat("/sbin/start-stop-daemon"); err != nil {
+		return false
+	}
+	return true
+}
+
+func isRedhatSysV() bool {
+	if _, err := os.Stat("/etc/rc.d/init.d/functions"); err != nil {
+		return false
+	}
+	return true
 }
